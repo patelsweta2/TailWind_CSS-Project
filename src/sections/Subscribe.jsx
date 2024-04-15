5c88114277296a40b5f42a2d858d6757c14fd92d
patelsweta2 <sweta0442@gmail.com>
2024-04-15 19:50:00 +0530

    complete the Subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,3 +1,5 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
   return (
     <section
@@ -5,10 +7,20 @@ const Subscribe = () => {
     items-center max-lg:flex-col gap-10"
       id="contact-us"
     >
-      <h3>
-        Sign up from
-        <span>Updates</span> & newsletter
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign up from {""}
+        <span className="text-coral-red">Updates</span> & newsletter
       </h3>
+      <div
+        className="lg:max-w-[40%] w-full flex 
+      items-center max-sm:flex-col gap-5 p-2.5 
+      sm:border sm:border-slate-gray rounded-full"
+      >
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end item-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
     </section>
   );
 };
