8c489d4b838568a146a357d1a23bc4fe1b01a238
patelsweta2 <sweta0442@gmail.com>
2024-04-15 18:26:22 +0530

    complete special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,20 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
     <button
-      className="flex justify-center items-center 
+      className={`flex justify-center items-center 
     gap-2 px-7 py-4 border 
-    font-montserrat text-lg leading-none bg-coral-red 
-    rounded-full text-white border-coral-red"
+    font-montserrat text-lg leading-none 
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+    } rounded-full`}
     >
       {label}
       {iconUrl && (
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,43 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1 ">
+        <img
+          src={offer}
+          alt=""
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red inline-block mt-3">Special</span>{" "}
+          Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals,quality comfort, and innovation for
+          your active life. Discover stylish Nike arrivals,quality comfort, and
+          innovation for your active life
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconUrl={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            border-color="border-slate-gray"
+            text-color="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
