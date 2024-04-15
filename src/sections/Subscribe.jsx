eea2113bd83091ccee4afef8bb6a61922f7974ae
patelsweta2 <sweta0442@gmail.com>
2024-04-15 19:39:20 +0530

    Complete customer reviews page

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,16 @@
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between 
+    items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3>
+        Sign up from
+        <span>Updates</span> & newsletter
+      </h3>
+    </section>
+  );
 };
 
 export default Subscribe;
